75ea52cd20f6bc71fb9a4a255d7ae8d1fb014b7e
YAMAMEYAMAYA <82656872+YAMAMEYAMAYA@users.noreply.github.com>
2021-04-16 22:00:13 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,6 +16,8 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-
+@bot.command()
+async def pp(ctx):
+    await ctx.send('にやーん')
 
 bot.run(token)
