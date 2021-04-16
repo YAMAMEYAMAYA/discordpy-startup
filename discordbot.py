379c7de666863f097edc7932d72bb29c0b505f95
YAMAMEYAMAYA <82656872+YAMAMEYAMAYA@users.noreply.github.com>
2021-04-16 21:47:48 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,4 +1,9 @@
-from discord.ext import commands
+def simple_dice(dice_size, dice_num):
+    dice_val = np.array([], dtype=np.int64)
+    for i in range(dice_num):
+        dice_val = np.append(dice_val, dice(dice_size))
+    msg = 'dice: ' + str(np.sum(dice_val)) + ' = ' + str(dice_val)
+    return msgfrom discord.ext import commands
 import os
 import traceback
 
@@ -17,5 +22,11 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
+def simple_dice(dice_size, dice_num):
+    dice_val = np.array([], dtype=np.int64)
+    for i in range(dice_num):
+        dice_val = np.append(dice_val, dice(dice_size))
+    msg = 'dice: ' + str(np.sum(dice_val)) + ' = ' + str(dice_val)
+    return msg    
 
 bot.run(token)
