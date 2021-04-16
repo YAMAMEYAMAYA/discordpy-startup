39b2ca23ab83afb975dee86c453e4dffabc36404
YAMAMEYAMAYA <82656872+YAMAMEYAMAYA@users.noreply.github.com>
2021-04-16 22:05:51 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,17 +5,16 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+def dice(dice_size):
+    num = np.random.randint(1, int(dice_size))
+    return num
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+def simple_dice(dice_size, dice_num):
+    dice_val = np.array([], dtype=np.int64)
+    for i in range(dice_num):
+        dice_val = np.append(dice_val, dice(dice_size))
+    msg = 'dice: ' + str(np.sum(dice_val)) + ' = ' + str(dice_val)
+    return msg
 
 
 bot.run(token)
