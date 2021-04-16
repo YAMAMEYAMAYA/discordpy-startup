89bdebb27ff37fdfa02d005cb4a8755eedc27678
YAMAMEYAMAYA <82656872+YAMAMEYAMAYA@users.noreply.github.com>
2021-04-16 21:54:31 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,9 +1,4 @@
-def simple_dice(dice_size, dice_num):
-    dice_val = np.array([], dtype=np.int64)
-    for i in range(dice_num):
-        dice_val = np.append(dice_val, dice(dice_size))
-    msg = 'dice: ' + str(np.sum(dice_val)) + ' = ' + str(dice_val)
-    return msgfrom discord.ext import commands
+from discord.ext import commands
 import os
 import traceback
 
